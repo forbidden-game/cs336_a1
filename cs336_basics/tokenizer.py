7cf029dffe021cba3482050119b47cdd7ff9dfdf
LucainPan <panxiezhao@gmail.com>
2025-07-21 13:30:25 +0800

    from here to debug&test

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,8 +1,6 @@
-import os
-import multiprocessing
 import json
 import regex as re
-
+from _collections_abc import Iterable, Iterator
 
 class tokenizer:
     def __init__(
@@ -15,64 +13,115 @@ class tokenizer:
         self.merges = merges
         self.special_tokens = special_tokens
         
+        if special_tokens:
+            bytes_special_tokens = [st.encode('utf-8') for st in special_tokens]
+            next_id = next(reversed(vocab.keys())) + 1
+            
+            for bst in bytes_special_tokens:
+                if bst not in self.vocab.values():
+                    self.vocab[next_id] = bst
+                    next_id += 1
+                    # for debug
+                    # print(f"current id is {next_id - 1}, bst is {bst}")
+                    
         
     def encode(
         self,
         text: str
     ) -> list[int]:
         
+        words = []
         PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-        words = re.findall(PAT, text)
+        
+        if self.special_tokens:
+            remove_tokens_pat = '|'.join(re.escape(token) for token in self.special_tokens)
+            for chunk in re.split(remove_tokens_pat, text):
+                if chunk:
+                    words.extend(re.findall(PAT, chunk))
+        else:
+            words = re.findall(PAT, text)
+        
+        # for bebug
+        print(words)
         
         text_token = []
         text_token_encode = []
         
         for word in words:
-            word_bytes = [bytes([c]) for c in word.encode('utf-8')]
-            
-            word_token = []
-            word_length = len(word_bytes)
-            if word_length == 1:
-                word_token = word_bytes
-            else:
+            word_token = [bytes([c]) for c in word.encode('utf-8')]
+            word_length = len(word_token)
+            print(f"word_length: {word_length}")
+            new_word_token = []
+            while word_length > 1:
                 i = 0
                 while i < word_length - 1:
-                    pair = (word_bytes[i], word_bytes[i+1])
-                    if pair in self.merges:                    
-                        # replace with new token
-                        new_token = pair[0] + pair[1]
-                        word_bytes[i+1] = new_token
-                        word_token.append(new_token)
+                    p1, p2 = word_token[i], word_token[i+1]
+                    print(f"i: {i}")
+                    print(f"fist two pair is: ({p1}, {p2})")
+                    if (p1, p2) in self.merges:
+                        new_word_token.append(p1+p2)
+                        i += 2
+                        print(f"They got paired!")
+                        print(f"i+2: {i}")
+                        if i == word_length - 1:
+                            new_word_token.append(word_token[-1])
                     else:
-                        word_token.append(word_bytes[i])
-                    
-                    i += 1
+                        new_word_token.append(p1)
+                        i += 1
+                        if i == word_length - 1:
+                            new_word_token.append(p2)
+                if word_token == new_word_token:
+                    break
+                word_token = new_word_token
+                print(f"word_token: {word_token}")
+                new_word_token = []
+                word_length = len(word_token)
+                # print(f"word_token: {word_token}")
+
             text_token.extend(word_token)
+                    
+
+        # print(f"text_token: {text_token}")
+        # print(f"len_text_token: {len(text_token)}")
         
-        for wt in text_token:
-            for t in wt:
-                for integer, vocab in self.vocab.items():
-                    if t == vocab:
-                       text_token_encode.append(integer)
+        # create tokens_to_ids for fast encoding
+        tokens_to_ids = {token: id for id, token in self.vocab.items()}
+        for token in text_token:
+            text_token_encode.append(tokens_to_ids[token])
         
-        return text_token_encode          
-            
-        
-        
+        return text_token_encode
+    
+    
     def encode_iterable(
         self,
         iterable: Iterable[str]
     ) -> Iterator[int]:
         
-        
+        iterator = iter(iterable)
+        tokens_to_ids = {token: id for id, token in self.vocab.items()}
+        for fisrt_str in iterator:
+            first_encode = self.encode(fisrt_str)
+            first_last_token = self.vocab[first_encode[-1]]
+            try:
+                second_str = next(iterator)
+                second_encode = self.encode(second_str)
+                second_last_token = self.vocab[second_encode[-1]]
+                if_cross_token = first_last_token + second_last_token
+                
+                if if_cross_token in tokens_to_ids.keys():
+                    for id in first_encode[:-1] + [tokens_to_ids[if_cross_token]] + second_encode[1:]:
+                        yield id
+            except StopIteration:
+                for id in first_encode:
+                    yield id         
+    
     def decode(
         self,
         ids: list[int]
     ) -> str:
         
-        return ""
-        
-
+        return "".join(self.vocab[id].decode('utf-8', errors='replace') for id in ids)
+    
 def from_files(
         cls,
         vocab_filepath: str,
@@ -81,12 +130,10 @@ def from_files(
     ) -> tokenizer:
 
         with open(vocab_filepath, 'r', encoding='utf-8') as f:
-            vocab = {index: token.encode('utf-8') for index, token in json.load(f).items()}
+            vocab = {int(index): bytes.fromhex(token) for index, token in json.load(f).items()}
         merges = []
         with open(merges_filepath, 'r', encoding='utf-8') as f:
             for p1, p2 in json.load(f):
-                merges.append((p1.encode('utf-8'), p2.encode('utf-8')))
+                merges.append((bytes.fromhex(p1), bytes.fromhex(p2)))
         
         return tokenizer(vocab, merges, special_tokens)
-                
-            
